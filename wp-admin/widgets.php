3a54af38cfb98bbf6299de12c276c2fcf59010f2
minhnhat1543 <106510729+minhnhat1543@users.noreply.github.com>
2022-06-27 15:45:35 +0700

    commit gh-page

---
+++ wp-admin/widgets.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://minhnhat1543.github.io/minhnhat_demo/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A7000%2Fmyproject%2Fwp-admin%2Fwidgets.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://minhnhat1543.github.io/minhnhat_demo/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A7000%2Fmyproject%2Fwp-admin%2Fwidgets.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://minhnhat1543.github.io/minhnhat_demo/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A7000%2Fmyproject%2Fwp-admin%2Fwidgets.php&reauth=1">https://minhnhat1543.github.io/minhnhat_demo/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A7000%2Fmyproject%2Fwp-admin%2Fwidgets.php&reauth=1</a></p>
+	</body>
+</html>
